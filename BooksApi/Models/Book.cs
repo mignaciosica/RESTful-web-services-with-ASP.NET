600b0b0cf19d040a99aa1968a8aa57f8b3835309
P4ssenger <mignacio.sica@gmail.com>
2021-03-04 15:31:36 -0300

    bookService

--- BooksApi/Models/Book.cs
+++ BooksApi/Models/Book.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace BooksApi.Models
 {
